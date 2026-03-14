daa71978fa3bb248ef45b5e296ea64431d003fd1
Ivan Kabanov <i.borkoff@yandex.ru>
2026-03-14 16:38:10 +0400

    Реализовал DriverFactory

--- automationTestingUIFramework/src/test/java/driver/DriverFactory.java
+++ automationTestingUIFramework/src/test/java/driver/DriverFactory.java
@@ -1,4 +1,46 @@
 package driver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
 public class DriverFactory {
+
+    private static WebDriver driver;
+
+    public static WebDriver getDriver(String browser) {
+
+        if (driver == null) {
+            switch (browser.toLowerCase()){
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+
+                default:
+                    throw new RuntimeException("Unsupported browser: " + browser);
+            }
+
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        }
+
+        return driver;
+    }
+
+    public static void quitDriver() {
+        if(driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+
 }
