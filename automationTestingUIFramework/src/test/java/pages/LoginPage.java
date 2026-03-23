12a2d3a85289add37b75ea673694f7e7b09b0b12
ivan.borkov <ivan.borkof@simbirsoft.com>
2026-03-23 16:54:33 +0300

    Добавил ПОМы на страницы для тестов для LoginPage

--- automationTestingUIFramework/src/test/java/pages/LoginPage.java
+++ automationTestingUIFramework/src/test/java/pages/LoginPage.java
@@ -1,4 +1,48 @@
 package pages;
 
-public class LoginPage {
+import core.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage {
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(id = "user-name")
+    private WebElement usernameInput;
+
+    @FindBy(id = "password")
+    private WebElement passwordInput;
+
+    @FindBy(id = "login-button")
+    private WebElement loginButton;
+
+    @FindBy(css = "[data-test='error']")
+    private WebElement errorMessage;
+
+    public void enterUsername(String username) {
+        usernameInput.sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        passwordInput.sendKeys(password);
+    }
+
+    public void clickLogin() {
+        loginButton.click();
+    }
+
+    public void login(String username, String password) {
+        enterUsername(username);
+        enterPassword(password);
+        clickLogin();
+    }
+
+    public String getErrorMessage() {
+        return errorMessage.getText();
+    }
 }
+
