79c7fae79d757404a93374f262131c9a74426695
ivan.borkov <ivan.borkof@simbirsoft.com>
2026-03-27 09:11:32 +0300

    Добавил аннотации

--- automationTestingUIFramework/src/test/java/pages/LoginPage.java
+++ automationTestingUIFramework/src/test/java/pages/LoginPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import core.BasePage;
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -23,24 +24,29 @@ public class LoginPage extends BasePage {
     @FindBy(css = "[data-test='error']")
     private WebElement errorMessage;
 
+    @Step("Ввод логина: {username}")
     public void enterUsername(String username) {
         usernameInput.sendKeys(username);
     }
 
+    @Step("Ввод пароля")
     public void enterPassword(String password) {
         passwordInput.sendKeys(password);
     }
 
+    @Step("Нажатие кнопки Login")
     public void clickLogin() {
         loginButton.click();
     }
 
+    @Step("Логин как пользователь: {username}")
     public void login(String username, String password) {
         enterUsername(username);
         enterPassword(password);
         clickLogin();
     }
 
+    @Step("Получение текста ошибки")
     public String getErrorMessage() {
         return errorMessage.getText();
     }
