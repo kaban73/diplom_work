06904d06448ab628a8ed53ae9af1d3729cd051ac
Ivan Kabanov <i.borkoff@yandex.ru>
2026-04-04 17:07:47 +0400

    Сделал грид, но чет хреново пашет. надо улучшить

--- automationTestingUIFramework/src/test/java/core/BaseTest.java
+++ automationTestingUIFramework/src/test/java/core/BaseTest.java
@@ -4,8 +4,10 @@ import driver.DriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import utils.ConfigReader;
 
+@Listeners({io.qameta.allure.testng.AllureTestNg.class})
 public class BaseTest {
 
     protected WebDriver driver;
